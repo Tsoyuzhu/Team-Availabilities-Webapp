6d314178c0bc60e197daa5520fef791308d2ce41
Tsoyuzhu <tsoyuzhu@gmail.com>
2018-06-25 17:36:05 +1000

    Find implemented. App has basic functionality

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var mongoose = require("mongoose");
 app = express();
 mongoose.connect("mongodb://localhost/hr_app");
 
-// This is a terrible schema structure. 
-// Edit: This is somewhat better. 
+// This schema holds the availabilities of a 
+// member in the system.
 
 var memberSchema = new mongoose.Schema({
 	name: String,
@@ -93,10 +93,10 @@ app.get('/edit', function (req, res) {
 });
 
 app.get('/edit/:id', function(req, res){
-	console.log("You sent the id "+req.params.id);
+	console.log("You want to edit the id "+req.params.id);
 	Member.findOne({_id: req.params.id}, function(err, member) {
 		if (err) {
-			console.log("FAILURE: Loading Existing members");
+			console.log("FAILURE: Could not load the member to edit.");
 		} else {
 			console.log(member);
 			res.render('editmem', {member, member});
@@ -105,123 +105,39 @@ app.get('/edit/:id', function(req, res){
 });
 app.post('/edit/:id', function(req, res){
 	console.log("You want to edit the id "+req.params.id);
+
+	// Process form submission
+	// Fill Schema
+	var name; 
+	var mon = [];
+	var tue = [];
+	var wed = [];
+	var thu = [];
+	var fri = [];
+	var sat = [];
+	var sun = [];
+
 	Member.findById(req.params.id, function(err, member) {
 		if (err) {
 			console.log("FAILURE: Could not find requested member to edit");
 		} else {
+			// As explained above for CREATE
+			mon = extractTimes (req.body.mon);
+			tue = extractTimes (req.body.tue);
+			wed = extractTimes (req.body.wed);
+			thu = extractTimes (req.body.thu);
+			fri = extractTimes (req.body.fri);
+			sat = extractTimes (req.body.sat);
+			sun = extractTimes (req.body.sun);
+
 			member.set({
-			mon8: req.body.submit.mon8,
-			mon9: req.body.submit.mon9,
-			mon10: req.body.submit.mon10,
-			mon11: req.body.submit.mon11,
-			mon12: req.body.submit.mon12,
-			mon13: req.body.submit.mon13,
-			mon14: req.body.submit.mon14,
-			mon15: req.body.submit.mon15,
-			mon16: req.body.submit.mon16,
-			mon17: req.body.submit.mon17,
-			mon18: req.body.submit.mon18,
-			mon19: req.body.submit.mon19,
-			mon20: req.body.submit.mon20,
-			mon21: req.body.submit.mon21,
-			mon22: req.body.submit.mon22,
-			mon23: req.body.submit.mon23,
-			tue8: req.body.submit.tue8,
-			tue9: req.body.submit.tue9,
-			tue10: req.body.submit.tue10,
-			tue11: req.body.submit.tue11,
-			tue12: req.body.submit.tue12,
-			tue13: req.body.submit.tue13,
-			tue14: req.body.submit.tue14,
-			tue15: req.body.submit.tue15,
-			tue16: req.body.submit.tue16,
-			tue17: req.body.submit.tue17,
-			tue18: req.body.submit.tue18,
-			tue19: req.body.submit.tue19,
-			tue20: req.body.submit.tue20,
-			tue21: req.body.submit.tue21,
-			tue22: req.body.submit.tue22,
-			tue23: req.body.submit.tue23,
-			wed8: req.body.submit.wed8,
-			wed9: req.body.submit.wed9,
-			wed10: req.body.submit.wed10,
-			wed11: req.body.submit.wed11,
-			wed12: req.body.submit.wed12,
-			wed13: req.body.submit.wed13,
-			wed14: req.body.submit.wed14,
-			wed15: req.body.submit.wed15,
-			wed16: req.body.submit.wed16,
-			wed17: req.body.submit.wed17,
-			wed18: req.body.submit.wed18,
-			wed19: req.body.submit.wed19,
-			wed20: req.body.submit.wed20,
-			wed21: req.body.submit.wed21,
-			wed22: req.body.submit.wed22,
-			wed23: req.body.submit.wed23,
-			thu8: req.body.submit.thu8,
-			thu9: req.body.submit.thu9,
-			thu10: req.body.submit.thu10,
-			thu11: req.body.submit.thu11,
-			thu12: req.body.submit.thu12,
-			thu13: req.body.submit.thu13,
-			thu14: req.body.submit.thu14,
-			thu15: req.body.submit.thu15,
-			thu16: req.body.submit.thu16,
-			thu17: req.body.submit.thu17,
-			thu18: req.body.submit.thu18,
-			thu19: req.body.submit.thu19,
-			thu20: req.body.submit.thu20,
-			thu21: req.body.submit.thu21,
-			thu22: req.body.submit.thu22,
-			thu23: req.body.submit.thu23,
-			fri8: req.body.submit.fri8,
-			fri9: req.body.submit.fri9,
-			fri10: req.body.submit.fri10,
-			fri11: req.body.submit.fri11,
-			fri12: req.body.submit.fri12,
-			fri13: req.body.submit.fri13,
-			fri14: req.body.submit.fri14,
-			fri15: req.body.submit.fri15,
-			fri16: req.body.submit.fri16,
-			fri17: req.body.submit.fri17,
-			fri18: req.body.submit.fri18,
-			fri19: req.body.submit.fri19,
-			fri20: req.body.submit.fri20,
-			fri21: req.body.submit.fri21,
-			fri22: req.body.submit.fri22,
-			fri23: req.body.submit.fri23,
-			sat8: req.body.submit.sat8,
-			sat9: req.body.submit.sat9,
-			sat10: req.body.submit.sat10,
-			sat11: req.body.submit.sat11,
-			sat12: req.body.submit.sat12,
-			sat13: req.body.submit.sat13,
-			sat14: req.body.submit.sat14,
-			sat15: req.body.submit.sat15,
-			sat16: req.body.submit.sat16,
-			sat17: req.body.submit.sat17,
-			sat18: req.body.submit.sat18,
-			sat19: req.body.submit.sat19,
-			sat20: req.body.submit.sat20,
-			sat21: req.body.submit.sat21,
-			sat22: req.body.submit.sat22,
-			sat23: req.body.submit.sat23,
-			sun8: req.body.submit.sun8,
-			sun9: req.body.submit.sun9,
-			sun10: req.body.submit.sun10,
-			sun11: req.body.submit.sun11,
-			sun12: req.body.submit.sun12,
-			sun13: req.body.submit.sun13,
-			sun14: req.body.submit.sun14,
-			sun15: req.body.submit.sun15,
-			sun16: req.body.submit.sun16,
-			sun17: req.body.submit.sun17,
-			sun18: req.body.submit.sun18,
-			sun19: req.body.submit.sun19,
-			sun20: req.body.submit.sun20,
-			sun21: req.body.submit.sun21,
-			sun22: req.body.submit.sun22,
-			sun23: req.body.submit.sun23 
+				mon: mon,
+				tue: tue,
+				wed: wed,
+				thu: thu,
+				fri: fri,
+				sat: sat,
+				sun: sun		
 			});
 		}
 		member.save(function (err, updatedMember) {
@@ -245,14 +161,24 @@ app.get('/find', function (req, res) {
 	});
 });
 
-app.get('/find/:id', function (req, res) {
-	Member.find({}, function(err, members) {
-		if (err) {
-			console.log("I couldn't find anything");
-		} else {
-			res.render('find', {members, members});
+app.post('/find/display', function (req, res) {
+	console.log("Received a display request")
+	var array = [];
+	for (var key in req.body.members ) {
+		if (req.body.members[key] == 1) {
+			array.push( mongoose.Types.ObjectId(key) );
 		}
+	};
+	//console.log(array);	
+	Member.find({'_id': { $in: array} }, function(err, reqmems){
+		if (err) {
+			console.log("Something went wrong with displaying members");
+		} else {
+	     	console.log(reqmems);
+	 		res.render('display', {reqmems:reqmems});
+ 		}
 	});
+	
 });
 
 //DESTROY
--- views/table.js
+++ views/table.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('td').click(function(){
+  $('td.clickable').click(function(){
   	$(this).toggleClass("trclicked");
 	if ( $(this).children().attr("value") == 0) {
   		$(this).children().attr("value",1);
